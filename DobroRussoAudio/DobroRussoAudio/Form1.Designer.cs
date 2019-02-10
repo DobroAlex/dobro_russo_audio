06008ca006e577d1f478dc519edb447f58e4058f
Alex <alexkoshernosiegov@gmail.com>
2019-02-10 21:06:16 +0300

    Working on docking and registration page form

--- DobroRussoAudio/DobroRussoAudio/Form1.Designer.cs
+++ DobroRussoAudio/DobroRussoAudio/Form1.Designer.cs
@@ -31,28 +31,40 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.DarkGreen;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
             this.label1.Font = new System.Drawing.Font("Tahoma", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1185, 84);
+            this.label1.Size = new System.Drawing.Size(1182, 84);
             this.label1.TabIndex = 0;
-            this.label1.Text = "ГИА-9. СОБЕСЕДОВАНИЕ ПО РУССКОМУ ЯЗЫКУ\r\nДоброверсия  1.0(8Feb19)";
+            this.label1.Text = "ГИА-9. СОБЕСЕДОВАНИЕ ПО РУССКОМУ ЯЗЫКУ\r\nДоброверсия  0.1(8Feb19)";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label2
             // 
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
             this.label2.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label2.Location = new System.Drawing.Point(0, 84);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(1185, 46);
+            this.label2.Size = new System.Drawing.Size(1182, 46);
             this.label2.TabIndex = 1;
             this.label2.Text = "Перед началом работы специалисту по работе с ПК необходимо задать параметры собес" +
     "едования";
@@ -61,13 +73,101 @@
             // label3
             // 
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.Location = new System.Drawing.Point(0, 130);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(1170, 102);
+            this.label3.Size = new System.Drawing.Size(1182, 102);
             this.label3.TabIndex = 2;
             this.label3.Text = "Настройка парметров собеседования";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 232);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1182, 275);
+            this.panel1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(262, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(907, 32);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(12, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(244, 50);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Код ОУ :";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(244, 52);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Название ОУ :";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 4);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(244, 53);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Город / Район :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(262, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(907, 32);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(262, 109);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(277, 32);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(563, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(301, 50);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Номер аудитории:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(870, 109);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(299, 32);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Leave += new System.EventHandler(this.textBox4_Leave);
             // 
             // RegistrationForm
             // 
@@ -75,12 +175,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "RegistrationForm";
             this.Text = "Форма регистрации";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -90,6 +193,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- DobroRussoAudio/DobroRussoAudio/Form1.cs
+++ DobroRussoAudio/DobroRussoAudio/Form1.cs
@@ -16,5 +16,41 @@ namespace DobroRussoAudio
         {
             InitializeComponent();
         }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            if (!textBox3.Text.All(char.IsDigit))
+            {
+                DialogResult dialogResult = MessageBox.Show("Вы ввели код ОУ, содержащий не только цифры. Мы считаем, что код ОУ долже быть численным. Вы уверенны, что хотите продолжить?", 
+                    "Подозритлеьный ввод кода ОУ", 
+                    MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.No)
+                {
+                    textBox3.Text = String.Empty;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        private void textBox4_Leave(object sender, EventArgs e)
+        {
+            if (!textBox4.Text.All(char.IsDigit))
+            {
+                DialogResult dialogResult = MessageBox.Show("Вы ввели номер ОУ, содержащий не только цифры. Мы считаем, что номер аудитоии долже быть численным. Вы уверенны, что хотите продолжить?",
+                    "Подозритлеьный ввод номера аудитории",
+                    MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.No)
+                {
+                    textBox4.Text = String.Empty;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
     }
 }
